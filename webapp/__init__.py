7d4e3709a4567446cf03e37b9d1d6245814a5963
Nikola Kurbalija <nikola.kurbalija@hotmail.com>
2021-07-06 12:57:51 +0200

    Removed unused import

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://nikola:asdasd@192.168.138.128:3306/flask-nikola"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:example@192.168.138.128:3306/flask-nikola"
 app.config["SECRET_KEY"] = "dcf8275b3b04f25160897be"
 db = SQLAlchemy(app)
 #Used to hash passwords

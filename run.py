d0536e93ffe0fb08f781c3760b8440d75bc881c8
Nikola Kurbalija <nikola.kurbalija@hotmail.com>
2021-09-13 09:56:12 +0200

    Updated to create test user  when started.

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 from webapp import app, db
+from webapp.models import User
 
 if __name__ == "__main__":
     db.create_all()
+    # Create an user for testing purposes
+    try:
+        db.session.add(
+            User(
+                username="test1234",
+                name="test1234",
+                email_address="test1234@mail.com",
+                password="asdasd",
+            )
+        )
+        db.session.commit()
+    except:
+        pass
     app.run(host="0.0.0.0", debug=True)

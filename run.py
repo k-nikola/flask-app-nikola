3a673d72e72d1f809745f1811834b13fe3b0f9ba
Nikola Kurbalija <nikola.kurbalija@hotmail.com>
2022-01-16 14:10:06 +0100

    Remove comment, and unused import.

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from webapp import app, db
-from webapp.models import User
 
 if __name__ == "__main__":
     db.create_all()
-    # Create an user for testing purposes
 
     app.run(host="0.0.0.0", debug=True)
